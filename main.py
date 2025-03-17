e89ae3dc6d84e7099de290ddb190421a1374cec5
ivanalfonso97 <ivanalfonso.chan@gmail.com>
2025-03-17 16:55:28 +0800

    limit framerate

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ def main():
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
         pygame.display.flip()
+
+        # Limits the framerate to 60FPS
+        time_passed = clock.tick(60)
+        dt = (time_passed / 1000)
 
 if __name__ == "__main__":
     main()
